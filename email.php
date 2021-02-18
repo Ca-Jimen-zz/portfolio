0bd368288a17e65abb1a39d022044127d6d789a8
Carlos Jimenez <ca.jimenez093@gmail.com>
2021-02-18 18:58:37 +0900

    adding contact functionality

--- email.php
+++ email.php
@@ -2,5 +2,8 @@
 if($_POST['message']) {
 mail("ca.jimenez093@gmail.com", $_POST['name'],
 $_POST['message'], "From: ". $_POST['email']);
+echo('Mail sent. Thank you ' . $_POST['name'] . ', I will get back to you shortly.')
+} else {
+    echo('Mail failed to send.')
 }
 ?>
