d3e07798864071784503a1875990f37c38da3c07
Carlos Jimenez <ca.jimenez093@gmail.com>
2021-02-24 19:10:54 +0900

    removed php file

--- email.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($_POST['message']) {
-mail("ca.jimenez093@gmail.com", $_POST['name'],
-$_POST['message'], "From: ". $_POST['email']);
-echo('Mail sent. Thank you ' . $_POST['name'] . ', I will get back to you shortly.')
-} else {
-    echo('Mail failed to send.')
-}
-?>
